cf5dfc5b05fce18826f2aaa7857cd837fbf9ba75
Caila Bridges <bridgescaila@gmail.com>
2024-02-16 18:25:48 -0500

    Organize and update index.js

--- src/index.js
+++ src/index.js
@@ -24,36 +24,40 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-let currentDateELement = document.querySelector("p #current-date");
-let currentDate = new Date();
-
-currentDateELement.innerHTML = formatDate(currentDate);
-
-function displayTemp(response) {
-  let currentTemp = Math.round(response.data.temperature.current);
-
-  let tempElement = document.querySelector("#current-temp");
-  tempElement.innerHTML = `${currentTemp}`;
+function getCurrentDate() {
+  let now = new Date();
+  return formatDate(now);
 }
 
-function searchInput(event) {
-  event.preventDefault();
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
 
-  let now = new Date();
-  let searchInput = document.querySelector("#search-input");
-  let currentCity = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
 
-  let currentDate = document.querySelector("p #current-date");
-  currentDate.innerHTML = formatDate(now);
-
-  // Weather API
-  let city = searchInput.value.toLowerCase();
-  currentCity.innerHTML = city.charAt(0).toUpperCase() + city.slice(1);
-
+function searchCity(city) {
   let apiKey = "241ff083e917bb12t439a7aco17d1be3";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(displayTemp);
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+
+  let currentDateElement = document.querySelector("#current-date");
+  currentDateElement.innerHTML = getCurrentDate();
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchInput);
+searchForm.addEventListener("submit", handleSearchSubmit);
+
+let currentDateElement = document.querySelector("#current-date");
+currentDateElement.innerHTML = getCurrentDate();
+
+searchCity("Raleigh");
