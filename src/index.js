e264620bf5fba3621b9c45878ecef0f3439527c3
Caila Bridges <bridgescaila@gmail.com>
2024-02-17 18:08:26 -0500

    Add svg weather icons to app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const customIcons = {
+  "clear sky": "<i class='wi wi-day-sunny'></i>",
+  "few clouds": "<i class='wi wi-day-cloudy'></i>",
+  "scattered clouds": "<i class='wi wi-day-cloudy-high'></i>",
+  "broken clouds": "<i class='wi wi-cloudy'></i>",
+  "overcast clouds": "<i class='wi wi-cloudy'></i>",
+  "shower rain": "<i class='wi wi-showers'></i>",
+  "rain": "<i class='wi wi-rain'></i>",
+  "thunderstorm": "<i class='wi wi-thunderstorm'></i>",
+  "snow": "<i class='wi wi-snow'></i>",
+  "mist": "<i class='wi wi-fog'></i>",
+  "haze": "<i class='wi wi-fog'></i>",
+};
+
+
 let days = [
   "Sunday",
   "Monday",
@@ -43,20 +58,31 @@ function displayWeather(response) {
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description")
   let humidityElement = document.querySelector("#humidity")
-  let windElement = document.querySelector("#wind")
+  let windElement = document.querySelector("#wind-speed")
+  let iconElement = document.querySelector("#current-temp-icon")
 
   let description = response.data.condition.description;
   description = capitalizeFirstLetter(description);
 
   let humidity = response.data.temperature.humidity;
   let wind = Math.round(response.data.wind.speed);
-  
+
+  let customIcon = customIcons[description.toLowerCase()];
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
   descriptionElement.innerHTML = description;
   humidityElement.innerHTML = `${humidity}%`;
   windElement.innerHTML = `${wind}km/h`;
+  
+
+  if (customIcon) {
+    iconElement.className = '';
+    iconElement.innerHTML = customIcon;
+  } else {
+    
+    iconElement.innerHTML = response.data.condition.icon_url; 
+  }
 }
 
 function searchCity(city) {
